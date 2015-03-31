28f553dded48f706fcf92ad264b9ca88a2debc82
perry home <voldemort76winxp@gmail.com>
2015-03-31 23:16:40 +0800

    find the center of a picture which include a circle

--- Lab1ver2/findCenter.m
+++ Lab1ver2/findCenter.m
@@ -2,7 +2,7 @@ function [x, y] = findCenter (Image)
 	
 	%given a surface of red of a picture
 	%we get a circle when we take a picture of bubble
-	%then we can find the center of light in (x,y)
+	%then we can find the center of light in terms of (x,y)
 
 	[row, col] = size(Image);
 	brightest = max(max(Image)');
