e2f395f137f6d054396b5bf9b6d946b91b66b748
perry home <voldemort76winxp@gmail.com>
2015-03-31 23:14:28 +0800

    find the center of a picture which include a circle

---
+++ Lab1ver2/findCenter.m
@@ -0,0 +1,16 @@
+function [x, y] = findCenter (Image)
+	
+	%given a surface of red of a picture
+	%we get a circle when we take a picture of bubble
+	%then we can find the center of light in (x,y)
+
+	[row, col] = size(Image);
+	brightest = max(max(Image)');
+	xSum = sum(Image == brightest);
+	ySum = sum(Image' == brightest);
+
+	x = find(xSum == max(xSum));
+	y = find(ySum == max(ySum));
+	x = x(1);
+	y = y(1);
+end
