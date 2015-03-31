b2955cbcd12189d337e224b994f97e81cd4c6314
voldemort92205 <voldemort92205@gmail.com>
2015-03-31 13:57:02 +0800

    fix some bug

--- Lab1/clearNoise.m
+++ Lab1/clearNoise.m
@@ -1,15 +1,18 @@
 function [color, width] = clearNoise (info, colorWidth)
+	%input :
+	%info : color type in order
+	%colorWidth : the width of each color 
+	
 	black = find(info == 0);
-	tail = size(black);
-	color = zeros(black(1)-1, 1);
-	width = zeros(black(1)-1, 1);
+	tail = size(black, 1);
+	color = linspace(0, 0, (tail-1));
+	width = linspace(0, 0, (tail-1));
 
 
-
-	for i = 1:(tail(1)-1)
+	for i = 1:(tail-1)
 		tmp = find(colorWidth((black(i)+1):(black(i+1)-1)) == max(colorWidth((black(i)+1):(black(i+1)-1))));
-		color(i) = info(black(i)+tmp);
-		width(i) = colorWidth(black(i)+tmp);
+		color(i) = info(black(i)+tmp(1));
+		width(i) = colorWidth(black(i)+tmp(1));
 	end
 
 end
