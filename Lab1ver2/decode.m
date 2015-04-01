09c02f9f70f3fb0c073b1e59c5b17d63b741d1c5
voldemort92205 <voldemort92205@gmail.com>
2015-04-01 22:00:12 +0800

    decode each picture

--- Lab1ver2/decode.m
+++ Lab1ver2/decode.m
@@ -1,12 +1,9 @@
 function [information, status] = decode (filename, threshold, status)
 
-
-	%status : if start get information or not
-
 	image = imread(filename);
 	[row, col, high] = size(image);
 	[x, y] = findCenter(image(:, :, 1));
-
+%for debug
 %	for i = 1:3
 %		subplot (2, 2, i);
 %		plot (image(:, y, i));
@@ -25,21 +22,8 @@ function [information, status] = decode (filename, threshold, status)
 	[green] = colorStripDetect (image(:, y, 2), threshold);	
 	[blue] = colorStripDetect (image(:, y, 3), threshold);	
 	
-%	subplot (2, 2, 1)
-%	plot (red);
-%	subplot (2, 2, 2)
-%	plot (green);
-%	subplot (2, 2, 3)
-%	plot (blue)
-
-	
 	
 	[color, width] = colorCombine(red, green, blue);
 	[signal] = detectSignal (color, width);
 	[status, information] = colorTransfere (color, width, status, signal);
-%	color 
-%	width
-%	printColor(color, width);
-%	information
-%	pause()
 end
