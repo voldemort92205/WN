dedf62e27be5b7ab8be4b2f12c9fc8bc7187493c
perry home <voldemort76winxp@gmail.com>
2015-04-01 08:10:56 +0800

    combine each color to rgb color mode

---
+++ Lab1ver2/colorCombine.m
@@ -0,0 +1,13 @@
+function [colorType, width] = colorCombine(r, g, b)
+	%for every pixel of r(red), g(green), b(blue)
+	%there are 7 kinds of combine, however if there are light, red always show up
+	%0 for dark, 1 for red
+	%4 for yellow (r+g), 5 for purple(r+b), 7 for white(r+g+b)
+	%colorType : represent every pixel's color type
+	%width : every color's width
+	%we need dark at first
+
+
+
+
+end
