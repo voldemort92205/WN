7117b8d4fb0ace4e457a8174fe1af84c93b487d7
voldemort92205 <voldemort92205@gmail.com>
2015-04-01 22:06:27 +0800

    print message

--- Lab1ver2/printInfo.m
+++ Lab1ver2/printInfo.m
@@ -2,15 +2,14 @@ function [information] = printInfo(info)
 	n = size(info, 2);
 	k = (n - mod(n, 4)) / 4;
 	for i = 1:k
-%		fprintf ('%d %d %d %d ,  ', info(4*i-3), info(4*i-2), info(4*i-1), info(4*i));
-%		fprintf ('')
+
 		tmp = info(4*i-3) + 4*info(4*i-2) + 16*info(4*i-1) + 64*info(4*i);
+%for debug		
+%		fprintf ('%d %d %d %d ,  ', info(4*i-3), info(4*i-2), info(4*i-1), info(4*i));
 %		fprintf ('%d => %c\n', tmp, char(tmp));
 	fprintf ('%c', tmp);
 	end
 
-	%n
-	%k
 	if 4*k == n
 		information = [];
 	else
