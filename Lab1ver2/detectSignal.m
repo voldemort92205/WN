f7c178ad1fcfcd05dab8c1e1003973e20ba09e90
perry home <voldemort76winxp@gmail.com>
2015-04-01 08:13:52 +0800

    detect some signal

---
+++ Lab1ver2/detectSignal.m
@@ -0,0 +1,8 @@
+function [answer] = detectSignal(width, status)
+	%check whether there are wider strip in the information
+	%if yes, answer is 1
+	%else answer is 0
+	%status means wheter getting information before
+	%if 
+
+end
