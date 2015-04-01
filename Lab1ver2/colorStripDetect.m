c124abcc2eafe2303637ea5bcc4034a9eb73d24c
perry home <voldemort76winxp@gmail.com>
2015-04-01 08:03:24 +0800

    transfere to bright or dark picture

---
+++ Lab1ver2/colorStripDetect.m
@@ -0,0 +1,9 @@
+function [gray] = colorStripDetect(image, threshold)
+	%transfere to dark or bright
+	%for every pixel, if the value is greater than or equal to threshold
+	%set 255
+	%else , set 0
+	
+
+
+end
